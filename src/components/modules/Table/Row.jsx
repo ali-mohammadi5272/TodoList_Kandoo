8c30fdaeb95983d4dae687810740e9df8cb140d3
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-02 13:42:26 +0330

    feat: create Table's Row

---
+++ src/components/modules/Table/Row.jsx
@@ -0,0 +1,24 @@
+import FontAwesomeIcon from "../FontawesomeIcon/FontawesomeIcon";
+
+const Row = () => {
+  return (
+    <div className="flex h-full w-full items-center justify-evenly text-sm hover:bg-gray-200">
+      <div className="flex w-full justify-center items-center">
+        <span>952900</span>
+      </div>
+      <div className="flex w-full justify-center items-center">
+        <span>چگونه انسان شاد تری باشیم؟</span>
+      </div>
+      <div className="flex w-full justify-center items-center  manage-buttons">
+        <button className="text-white text-md rounded-md py-2 px-3 bg-red-500 hover:bg-red-600">
+          <FontAwesomeIcon icon="faTrashCan" />
+        </button>
+        <button className="text-white text-md rounded-md py-2 px-3 bg-[#008f8fcc] hover:bg-[#007272cc]">
+          <FontAwesomeIcon icon="faPenToSquare" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Row;
