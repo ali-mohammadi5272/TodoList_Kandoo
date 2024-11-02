618587d4325602f23700b60c1c6c7eb1015d3656
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-02 23:19:22 +0330

    style: padding

--- src/components/modules/Table/Button.jsx
+++ src/components/modules/Table/Button.jsx
@@ -3,7 +3,7 @@ import FontAwesomeIcon from "../FontawesomeIcon/FontawesomeIcon";
 const Button = ({ className, onClick, icon }) => {
   return (
     <button
-      className={`text-white text-md rounded-md pt-1 px-3 pb-2 ${
+      className={`text-white text-md rounded-md py-1 px-3 ${
         className ? className : ""
       }`}
       onClick={onClick}
