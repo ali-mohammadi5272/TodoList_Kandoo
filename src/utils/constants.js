c87d0b17cc30de0fd2498913eedd64a136fb0e10
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-03 01:42:19 +0330

    feat: add todosMessages to constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,4 +7,17 @@ const dbDefaultTables = {
   todos: [],
 };
 
-export { themes, dbDefaultTables };
+const todosMessages = {
+  success: {
+    create: "Todo created successfully",
+    update: "Todo updated successfully",
+    remove: "Todo removed successfully",
+  },
+  error: {
+    create: "Create Todo failed. Todo already exists !!",
+    update: "Update Todo failed !!",
+    remove: "Remove Todo successfully !!",
+  },
+};
+
+export { themes, dbDefaultTables, todosMessages };
