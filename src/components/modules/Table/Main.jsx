16feebeef93809c578d311e680118158edebc7bd
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-02 16:11:39 +0330

    feat: Use store's todos

--- src/components/modules/Table/Main.jsx
+++ src/components/modules/Table/Main.jsx
@@ -1,9 +1,24 @@
+import { useSelector } from "react-redux";
 import Row from "./Row";
 
 const Main = () => {
+  const todos = useSelector((state) => state.todos);
+
   return (
     <div className="table-main">
-      <Row />
+      {todos.length > 0 ? (
+        todos.map((todo, index) => (
+          <Row
+            key={todo.id}
+            id={todo.id}
+            title={todo.title}
+            isCompleted={todo.isCompleted}
+            index={index}
+          />
+        ))
+      ) : (
+        <p className="text-center py-1">No Content</p>
+      )}
     </div>
   );
 };
