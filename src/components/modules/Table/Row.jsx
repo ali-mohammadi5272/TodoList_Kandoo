bbbdc7516fdf651cf920fadab46fdfe3a3946f3d
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-03 02:10:33 +0330

    feat: create removeTodoHandler function

--- src/components/modules/Table/Row.jsx
+++ src/components/modules/Table/Row.jsx
@@ -1,19 +1,29 @@
 import { useState } from "react";
 import FontAwesomeIcon from "../FontawesomeIcon/FontawesomeIcon";
+import swal from "sweetalert";
 import Button from "./Button";
+import { useDispatch } from "react-redux";
+import { removeOne } from "./../../../Redux/stores/Todos";
+import { toast } from "react-toastify";
+import { todosMessages } from "../../../utils/constants";
 
 const Row = ({ id, title, isCompleted, index }) => {
   const [canEdit, setCanEdit] = useState(false);
   const [editedTodo, setEditedTodo] = useState("");
+  const dispatch = useDispatch();
 
-  const changeCanEditStatus = () => setCanEdit((prev) => !prev);
-
-  const editTodoTitle = (e) => {
-    setEditedTodo(e.target.textContent);
-  };
-
-  const editBtnHandler = () => {
-    changeCanEditStatus();
+  const removeTodoHandler = async () => {
+    const result = await swal({
+      title: "Remove Todo",
+      icon: "warning",
+      text: `Remove ${title} ?`,
+      buttons: ["No", "Yes"],
+    });
+    setEditedTodo("");
+    if (result) {
+      dispatch(removeOne(id));
+      toast.success(todosMessages.success.remove);
+    }
   };
 
   return (
@@ -28,7 +38,6 @@ const Row = ({ id, title, isCompleted, index }) => {
       <div
         className="w-full text-center pt-1 pb-2 max-w-full overflow-auto"
         contentEditable={canEdit}
-        onInput={editTodoTitle}
       >
         <span className="whitespace-nowrap">
           {editedTodo.trim() ? editedTodo : title}
@@ -57,10 +66,13 @@ const Row = ({ id, title, isCompleted, index }) => {
         )}
       </div>
       <div className="flex w-full justify-center items-center">
-        <Button className="bg-red-500 hover:bg-red-600" icon="faTrashCan" />
+        <Button
+          className="bg-red-500 hover:bg-red-600"
+          onClick={removeTodoHandler}
+          icon="faTrashCan"
+        />
         <Button
           className="bg-[#008f8fcc] hover:bg-[#007272cc]"
-          onClick={editBtnHandler}
           icon="faPenToSquare"
         />
       </div>
