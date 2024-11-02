4551c1d2e66f9355ff7383d2a88b34d05dc388b6
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-02 13:46:18 +0330

    style: Row icons padding

--- src/components/modules/Table/Row.jsx
+++ src/components/modules/Table/Row.jsx
@@ -10,10 +10,10 @@ const Row = () => {
         <span>چگونه انسان شاد تری باشیم؟</span>
       </div>
       <div className="flex w-full justify-center items-center  manage-buttons">
-        <button className="text-white text-md rounded-md py-2 px-3 bg-red-500 hover:bg-red-600">
+        <button className="text-white text-md rounded-md pt-1 pb-2 px-3 bg-red-500 hover:bg-red-600">
           <FontAwesomeIcon icon="faTrashCan" />
         </button>
-        <button className="text-white text-md rounded-md py-2 px-3 bg-[#008f8fcc] hover:bg-[#007272cc]">
+        <button className="text-white text-md rounded-md pt-1 pb-2 px-3 bg-[#008f8fcc] hover:bg-[#007272cc]">
           <FontAwesomeIcon icon="faPenToSquare" />
         </button>
       </div>
