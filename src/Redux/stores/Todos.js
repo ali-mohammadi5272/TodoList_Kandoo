6e3f4fe01069dc5f1edc28a5beb886acde21502e
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-02 17:10:16 +0330

    feat: todos initial state

--- src/Redux/stores/Todos.js
+++ src/Redux/stores/Todos.js
@@ -1,8 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getLocalStorageData } from "../../utils/helperFunctions";
+import { dbDefaultTables } from "../../utils/constants";
+
+const { todos } = getLocalStorageData("db") || dbDefaultTables;
 
 const slice = createSlice({
   name: "Todos",
-  initialState: [],
+  initialState: todos,
   reducers: {},
 });
 
