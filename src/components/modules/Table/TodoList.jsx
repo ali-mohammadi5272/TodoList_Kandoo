36d091d5f8589feb6759dc118ddec28b66f3be2e
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-03 00:58:35 +0330

    feat: Create TodoList Component

---
+++ src/components/modules/Table/TodoList.jsx
@@ -0,0 +1,13 @@
+import TodoCreator from "../TodoCreator/TodoCreator";
+import Table from "./Table";
+
+const TodoList = () => {
+  return (
+    <>
+      <TodoCreator />
+      <Table />
+    </>
+  );
+};
+
+export default TodoList;
