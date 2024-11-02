dc0838d6878127fd73893f9290403aa596a9ab57
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-02 13:43:47 +0330

    feat: create Table component

---
+++ src/components/modules/Table/Table.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+
+const Table = () => {
+  return (
+    <div className="bg-white overflow-hidden rounded-md w-full">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+};
+
+export default Table;
