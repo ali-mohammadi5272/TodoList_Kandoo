b59aa26900cb885c535b99c2c10e58315d517e1a
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-03 01:49:22 +0330

    feat: create inputChange function

--- src/components/modules/Table/TodoCreator.jsx
+++ src/components/modules/Table/TodoCreator.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 const TodoCreator = () => {
+  const [todo, setTodo] = useState("");
+
+  const inputChange = (e) => setTodo(e.target.value);
+
   return (
     <form action="" className="py-4">
       <div className="flex bg-slate-200 rounded-3xl overflow-hidden hover:bg-slate-300">
@@ -7,6 +13,9 @@ const TodoCreator = () => {
           type="text"
           id="input-box"
           placeholder="add your tasks"
+          value={todo}
+          onChange={inputChange}
+          required
         />
         <input
           className="bg-orange-500 cursor-pointer text-white font-bold text-lg rounded-3xl py-1 px-6 hover:bg-orange-600 tracking-wide disabled:bg-slate-500"
