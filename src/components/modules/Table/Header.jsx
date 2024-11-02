e497828ed5bc658d38e1937cd67779f47b8999e8
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-02 13:47:11 +0330

    style: Header font

--- src/components/modules/Table/Header.jsx
+++ src/components/modules/Table/Header.jsx
@@ -1,13 +1,13 @@
 const Header = () => {
   return (
     <header className="flex items-center justify-evenly h-12  text-sm text-[gray] bg-[#fcfcfc] select-none border-b-[1px] border-b-[gray]">
-      <div className="flex items-center justify-center w-full">
+      <div className="flex font-bold text-lg items-center justify-center w-full">
         <span>ID</span>
       </div>
-      <div className="flex items-center justify-center w-full">
+      <div className="flex font-bold text-lg items-center justify-center w-full">
         <span>Title</span>
       </div>
-      <div className="flex items-center justify-center w-full">
+      <div className="flex font-bold text-lg items-center justify-center w-full">
         <span>#</span>
       </div>
     </header>
