c97c18a0691b72401d5c2212ef55c7bcb29f4540
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-02 16:51:52 +0330

    feat: change Table's Row Component and make it dynamic

--- src/components/modules/Table/Row.jsx
+++ src/components/modules/Table/Row.jsx
@@ -1,21 +1,68 @@
+import { useState } from "react";
 import FontAwesomeIcon from "../FontawesomeIcon/FontawesomeIcon";
+import Button from "./Button";
 
-const Row = () => {
+const Row = ({ id, title, isCompleted, index }) => {
+  const [canEdit, setCanEdit] = useState(false);
+  const [editedTodo, setEditedTodo] = useState("");
+
+  const changeCanEditStatus = () => setCanEdit((prev) => !prev);
+
+  const editTodoTitle = (e) => {
+    setEditedTodo(e.target.textContent);
+  };
+
+  const editBtnHandler = () => {
+    changeCanEditStatus();
+  };
+
   return (
-    <div className="flex h-full w-full items-center justify-evenly text-sm hover:bg-gray-200 py-1">
-      <div className="w-full text-center pt-1 pb-2 px-3">
-        <span>952900</span>
+    <div
+      className={`flex h-full w-full items-center justify-evenly text-sm hover:bg-gray-200 py-2 ${
+        canEdit ? "bg-gray-300" : ""
+      }`}
+    >
+      <div className=" w-2/4 text-center pt-1 pb-2 max-w-2/4 overflow-auto">
+        <span className="whitespace-nowrap">{index + 1}</span>
       </div>
-      <div className="w-full text-center pt-1 pb-2 px-3">
-        <span>چگونه انسان شاد تری باشیم؟</span>
+      <div
+        className="w-full text-center pt-1 pb-2 max-w-full overflow-auto"
+        contentEditable={canEdit}
+        onInput={editTodoTitle}
+      >
+        <span className="whitespace-nowrap">
+          {editedTodo.trim() ? editedTodo : title}
+        </span>
       </div>
       <div className="flex w-full justify-center items-center">
-        <button className="text-white text-md rounded-md pt-1 pb-2 px-3 bg-red-500 hover:bg-red-600">
-          <FontAwesomeIcon icon="faTrashCan" />
-        </button>
-        <button className="text-white text-md rounded-md pt-1 pb-2 px-3 bg-[#008f8fcc] hover:bg-[#007272cc]">
-          <FontAwesomeIcon icon="faPenToSquare" />
-        </button>
+        {canEdit && (
+          <>
+            <Button className="bg-red-500 hover:bg-red-600" icon="faXmark" />
+            <Button
+              className="bg-green-500 hover:bg-green-600"
+              icon="faCheck"
+            />
+          </>
+        )}
+        {!canEdit && (
+          <span
+            className={`inline-block scale-150 ${
+              isCompleted ? "text-green-500 " : "text-red-500"
+            }`}
+          >
+            <FontAwesomeIcon
+              icon={isCompleted ? "faCircleCheck" : "faCircleXmark"}
+            />
+          </span>
+        )}
+      </div>
+      <div className="flex w-full justify-center items-center">
+        <Button className="bg-red-500 hover:bg-red-600" icon="faTrashCan" />
+        <Button
+          className="bg-[#008f8fcc] hover:bg-[#007272cc]"
+          onClick={editBtnHandler}
+          icon="faPenToSquare"
+        />
       </div>
     </div>
   );
