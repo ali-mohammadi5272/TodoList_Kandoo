1dbba0a2a70090f25847a3a7c708c6a539b773bd
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-02 16:23:17 +0330

    refactor: change Column's name

--- src/components/modules/Table/Header.jsx
+++ src/components/modules/Table/Header.jsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
     <header className="flex items-center justify-evenly h-12  text-sm text-[gray] bg-[#fcfcfc] select-none border-b-[1px] border-b-[gray]">
-      <div className="flex font-bold text-lg items-center justify-center w-full">
-        <span>ID</span>
+      <div className="flex font-bold text-lg items-center justify-center w-2/4">
+        <span>#</span>
       </div>
       <div className="flex font-bold text-lg items-center justify-center w-full">
         <span>Title</span>
@@ -11,7 +11,7 @@ const Header = () => {
         <span>Done</span>
       </div>
       <div className="flex font-bold text-lg items-center justify-center w-full">
-        <span>#</span>
+        <span>Delete/Edit</span>
       </div>
     </header>
   );
