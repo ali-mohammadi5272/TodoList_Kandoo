0a6287b0d7f508e4259532e37650e8dfed7613f8
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-02 23:55:32 +0330

    style: background-color and text color

--- src/components/modules/Table/Header.jsx
+++ src/components/modules/Table/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="flex items-center justify-evenly h-12  text-sm text-[gray] bg-[#fcfcfc] select-none border-b-[1px] border-b-[gray]">
+    <header className="flex items-center justify-evenly h-12  text-sm text-gray-700 bg-slate-100 select-none border-b-[1px] border-b-[gray]">
       <div className="flex font-bold text-lg items-center justify-center w-2/4">
         <span>#</span>
       </div>
