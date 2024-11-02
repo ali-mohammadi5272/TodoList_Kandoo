f0d9f8122f30aca9d3d0d4ec77af86ae547ab603
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-03 01:21:24 +0330

    feat: complete create ActionType

--- src/Redux/stores/Todos.js
+++ src/Redux/stores/Todos.js
@@ -1,6 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { getLocalStorageData } from "../../utils/helperFunctions";
 import { dbDefaultTables } from "../../utils/constants";
+import {
+  createTodo,
+  getLocalStorageData,
+  setLocalStorageData,
+} from "../../utils/helperFunctions";
 
 const { todos } = getLocalStorageData("db") || dbDefaultTables;
 
@@ -8,7 +12,13 @@ const slice = createSlice({
   name: "Todos",
   initialState: todos,
   reducers: {
-    create: (state, action) => {},
+    create: (state, action) => {
+      const newTodo = createTodo(action.payload);
+      const db = getLocalStorageData("db") || dbDefaultTables;
+      db.todos.push(newTodo);
+      setLocalStorageData("db", db);
+      state.push(newTodo);
+    },
     updateTitle: (state, action) => {},
     updateStatus: (state, action) => {},
     removeOne: (state, action) => {},
