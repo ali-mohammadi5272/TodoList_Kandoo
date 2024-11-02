733b7fbf281937528166bd1344bd38a40beeb5a9
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-02 18:14:25 +0330

    fix: fix Outlet bug

--- src/components/templates/Layout/Layout.jsx
+++ src/components/templates/Layout/Layout.jsx
@@ -1,8 +1,11 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
-  return <div>{Outlet}</div>;
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 };
 
 export default Layout;
