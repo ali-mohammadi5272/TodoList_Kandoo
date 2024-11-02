d9a1531089bb0f21d3a1ba99a37cf112bac54830
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-02 13:57:13 +0330

    style: Row classNames

--- src/components/modules/Table/Row.jsx
+++ src/components/modules/Table/Row.jsx
@@ -3,13 +3,13 @@ import FontAwesomeIcon from "../FontawesomeIcon/FontawesomeIcon";
 const Row = () => {
   return (
     <div className="flex h-full w-full items-center justify-evenly text-sm hover:bg-gray-200 py-1">
-      <div className="flex w-full justify-center items-center">
+      <div className="w-full text-center">
         <span>952900</span>
       </div>
-      <div className="flex w-full justify-center items-center">
+      <div className="w-full text-center">
         <span>چگونه انسان شاد تری باشیم؟</span>
       </div>
-      <div className="flex w-full justify-center items-center  manage-buttons">
+      <div className="flex w-full justify-center items-center">
         <button className="text-white text-md rounded-md pt-1 pb-2 px-3 bg-red-500 hover:bg-red-600">
           <FontAwesomeIcon icon="faTrashCan" />
         </button>
