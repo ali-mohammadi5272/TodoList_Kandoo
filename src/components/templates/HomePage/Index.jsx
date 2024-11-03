a556b71ac342ce48917b48cd30c01471c4789626
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-03 05:02:39 +0330

    refactor: remove Container Ccomponent

--- src/components/templates/HomePage/Index.jsx
+++ src/components/templates/HomePage/Index.jsx
@@ -1,5 +1,4 @@
 import { useLayoutEffect } from "react";
-import Container from "../../modules/Container/Container";
 import TodoList from "../../modules/TodoList/TodoList";
 
 const HomePage = () => {
@@ -9,12 +8,10 @@ const HomePage = () => {
 
   return (
     <main>
-      <Container>
-        <h1 className="text-5xl font-bold my-5 text-center">Home</h1>
-        <section>
-          <TodoList />
-        </section>
-      </Container>
+      <h1 className="text-5xl font-bold my-5 text-center">Home</h1>
+      <section>
+        <TodoList />
+      </section>
     </main>
   );
 };
