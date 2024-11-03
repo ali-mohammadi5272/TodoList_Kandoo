732d79ed6e6a487311817e9f0312bcb07fb442f5
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-03 04:34:07 +0330

    feat: Page title

--- src/components/templates/HomePage/Index.jsx
+++ src/components/templates/HomePage/Index.jsx
@@ -1,7 +1,12 @@
+import { useLayoutEffect } from "react";
 import Container from "../../modules/Container/Container";
 import TodoList from "../../modules/TodoList/TodoList";
 
 const HomePage = () => {
+  useLayoutEffect(() => {
+    document.title = "Home";
+  }, []);
+
   return (
     <main>
       <Container>
