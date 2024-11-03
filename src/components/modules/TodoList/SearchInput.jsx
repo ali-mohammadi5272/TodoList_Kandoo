fda19983b3f86d97c53643d4340b7490a25367ea
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-03 11:32:00 +0330

    feat: create SearchInput Component

---
+++ src/components/modules/TodoList/SearchInput.jsx
@@ -0,0 +1,21 @@
+const SearchInput = ({ onChange, value }) => {
+  const submitFormHandler = (e) => e.preventDefault();
+
+  return (
+    <form action="" className="py-4" onSubmit={submitFormHandler}>
+      <div className="flex bg-slate-200 rounded-3xl hover:bg-slate-300">
+        <input
+          className="py-1 outline-none px-3 border-none bg-transparent w-full rounded-3xl"
+          type="search"
+          id="input-box"
+          placeholder="find Todo"
+          value={value}
+          onChange={onChange}
+          required
+        />
+      </div>
+    </form>
+  );
+};
+
+export default SearchInput;
