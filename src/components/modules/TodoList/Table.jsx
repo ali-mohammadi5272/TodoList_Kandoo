b21130f4e067d7602a6e854e7762e71d9adb43b2
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-03 11:46:24 +0330

    feat: add SearchFilter

--- src/components/modules/TodoList/Table.jsx
+++ src/components/modules/TodoList/Table.jsx
@@ -1,12 +1,30 @@
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
+import SearchInput from "./SearchInput";
+import { useSelector } from "react-redux";
+import { useState } from "react";
 
 const Table = () => {
+  const todos = useSelector((state) => state.todos) || [];
+  const [filteredTodos, setFilteredTodos] = useState(null);
+
+  const searchInputChange = (e) => {
+    if (e.target.value.trim()) {
+      const filteredData = todos.filter((todo) =>
+        todo.title.trim().includes(e.target.value.trim())
+      );
+      setFilteredTodos(filteredData);
+    } else {
+      setFilteredTodos(null);
+    }
+  };
+
   return (
     <div className="bg-slate-200 overflow-hidden rounded-lg w-full">
+      <SearchInput onChange={searchInputChange} />
       <Header />
-      <Main />
+      <Main todos={filteredTodos ? filteredTodos : todos} />
       <Footer />
     </div>
   );
