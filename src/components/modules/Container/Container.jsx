ffc13e2803b57a3565998155af64548095dfb622
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-03 06:02:10 +0330

    style: Container Padding

--- src/components/modules/Container/Container.jsx
+++ src/components/modules/Container/Container.jsx
@@ -1,5 +1,5 @@
 const Container = ({ children }) => {
-  return <div className="m-auto max-w-2xl">{children}</div>;
+  return <div className="m-auto max-w-2xl px-4">{children}</div>;
 };
 
 export default Container;
