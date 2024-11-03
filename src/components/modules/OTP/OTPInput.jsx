73ad663a318da2e577d92b5a3b104d67fd488b5a
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-03 11:57:27 +0330

    style: OTP's inputs bg-color

--- src/components/modules/OTP/OTPInput.jsx
+++ src/components/modules/OTP/OTPInput.jsx
@@ -2,7 +2,7 @@ const OTPInput = ({ refElement, onChange }) => {
   return (
     <input
       type="number"
-      className="py-2 w-full text-3xl text-center rounded-lg outline-none"
+      className="py-2 w-full text-3xl text-center rounded-lg outline-none bg-slate-200"
       max={9}
       min={0}
       minLength={1}
