61c2fa76eb4cb281ca6e707abc9e55a4ca000924
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-03 05:03:07 +0330

    feat: add Navbar

--- src/components/templates/Layout/Layout.jsx
+++ src/components/templates/Layout/Layout.jsx
@@ -1,9 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../../modules/Navbar/Navbar";
+import Container from "../../modules/Container/Container";
 
 const Layout = () => {
   return (
     <div>
-      <Outlet />
+      <Navbar />
+      <Container>
+        <Outlet />
+      </Container>
     </div>
   );
 };
