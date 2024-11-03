c94a7976f1ebea3f0622e4cefe891f8c16121c50
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-11-03 11:52:08 +0330

    feat: add SearchIcon

--- src/components/modules/TodoList/SearchInput.jsx
+++ src/components/modules/TodoList/SearchInput.jsx
@@ -1,20 +1,24 @@
+import FontAwesomeIcon from "../FontawesomeIcon/FontawesomeIcon";
+
 const SearchInput = ({ onChange }) => {
   const submitFormHandler = (e) => e.preventDefault();
 
   return (
     <form action="" className="py-4" onSubmit={submitFormHandler}>
-      <div className="flex bg-slate-200 rounded-3xl hover:bg-slate-300">
+      <div className="flex relative bg-slate-200 rounded-3xl hover:bg-slate-300">
         <input
-          className="py-1 outline-none px-3 border-none bg-transparent w-full rounded-3xl"
+          className="py-1  outline-none px-3 pl-9 border-none bg-transparent w-full rounded-3xl"
           type="search"
           id="input-box"
           placeholder="find Todo"
           onChange={onChange}
           required
         />
+        <span className="absolute top-1 left-3">
+          <FontAwesomeIcon icon="faMagnifyingGlass" />
+        </span>
       </div>
     </form>
   );
 };
-
 export default SearchInput;
